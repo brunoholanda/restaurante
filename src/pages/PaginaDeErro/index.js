2bddf03c5cdc8d8ab244d805a9f25c7b14d58433
Bruno Holanda <holanda_rodrigues@hotmail.com>
2023-02-06 20:11:02 -0300

    Add files via upload

--- src/pages/PaginaDeErro/index.js
+++ src/pages/PaginaDeErro/index.js
@@ -1,11 +1,21 @@
 import styles from './PaginaDeErro.module.css';
 import erro404 from './404.png';
+import BotaoLaranja from 'components/BotaoLaranja';
+import { useNavigate } from 'react-router-dom';
 
 function PaginaDeErro() {
+
+    const navegar = useNavigate ();
+
     return (
         <section className={styles.erro}>
-            <img src={erro404} alt="imagem da pagina de erro"/>
-            <h1>Ops... Pagina não encontrada... Clique em voltar!</h1>
+            <img src={erro404} alt="imagem da pagina de erro" />
+            <div>
+                <h1>Ops... Pagina não encontrada... Clique em voltar!</h1>
+                <div onClick={() => navegar(-1)}>
+                    <BotaoLaranja>VOLTAR</BotaoLaranja>
+                </div>
+            </div>
         </section>
     )
 }
